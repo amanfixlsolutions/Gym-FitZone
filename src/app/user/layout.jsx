69fdd810d18422ac4aed81985e2d7b068a8a92f8
amanfixlsolutions <amanfixlsolutions@gmail.com>
2026-05-09 13:04:23 +0530

    fix: add Blog to user/layout.jsx NAV_ITEMS - this is the actual navbar for /user routes

--- src/app/user/layout.jsx
+++ src/app/user/layout.jsx
@@ -11,6 +11,7 @@ const NAV_ITEMS = [
   { label: "Classes",    href: "/user/classes" },
   { label: "Trainers",   href: "/user/trainers" },
   { label: "Membership", href: "/user/membership" },
+  { label: "Blog",       href: "/blog" },
   { label: "Contact",    href: "/user/contact" },
 ];
 
